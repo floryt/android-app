dc7803d4dcc6241d0d81eb4bc3b24e675f5c5889
Steven Dashevsky <stiven1999d@gmail.com>
2017-05-01 14:34:04 +0300

    Cleaned up code in the notification screen. Extracted verification flag sending to Common class

--- app/src/main/java/com/floryt/app/NotificationActivity.java
+++ app/src/main/java/com/floryt/app/NotificationActivity.java
@@ -10,26 +10,33 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.BitmapImageViewTarget;
+import com.floryt.common.Common;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.HashMap;
 
 public class NotificationActivity extends AppCompatActivity {
+    HashMap<String, String> data;
+    boolean selected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
-        HashMap<String, String> data = (HashMap<String, String>) getIntent().getSerializableExtra("data");
+        //noinspection unchecked
+        data = (HashMap<String, String>) getIntent().getSerializableExtra("data");
+        final String verificationUID = data.get("verificationUID");
 
         final ImageView userImageImageView = (ImageView) findViewById(R.id.userIcon);
         TextView userNameTextView = (TextView) findViewById(R.id.userName);
         TextView userEmailTextView = (TextView) findViewById(R.id.userEmail);
         TextView textLogo = (TextView)findViewById(R.id.text_logo);
+
         Typeface custom_font = Typeface.createFromAsset(getAssets(),  "fonts/CFDiamond.ttf");
         textLogo.setTypeface(custom_font);
 
@@ -51,27 +58,37 @@ public class NotificationActivity extends AppCompatActivity {
         Button yesButton = (Button) findViewById(R.id.yesButton);
         Button noButton = (Button) findViewById(R.id.noButton);
 
-        View.OnClickListener uploadVerification = new View.OnClickListener() {
+        final View.OnClickListener uploadVerification = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 boolean value;
+                selected = true;
                 switch (v.getId()) {
                     case R.id.yesButton:
-                        value = true;
+                        uploadIdentityAnswer(true);
                         break;
                     case R.id.noButton:
                     default:
-                        value = false;
+                        uploadIdentityAnswer(false);
                 }
-                FirebaseDatabase.getInstance()
-                        .getReference("IdentityVerifications")
-                        .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
-                        .setValue(value);
                 finish();
             }
         };
 
         yesButton.setOnClickListener(uploadVerification);
         noButton.setOnClickListener(uploadVerification);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(selected) return;
+        uploadIdentityAnswer(false);
+        finish();
+    }
+
+    private void uploadIdentityAnswer(boolean isApproved){
+        // TODO add timeout
+        Common.uploadIdentityAnswer(getApplicationContext(), isApproved, data.get("verificationUID"));
     }
 }
--- app/src/main/java/com/floryt/common/Common.java
+++ app/src/main/java/com/floryt/common/Common.java
@@ -1,5 +1,9 @@
 package com.floryt.common;
 
+import android.content.Context;
+import android.widget.Toast;
+
+import com.floryt.app.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.iid.FirebaseInstanceId;
@@ -18,17 +22,30 @@ public class Common {
     private Common() {
     }
 
-    public void saveToken(){
-        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+    public void saveToken(){ //TODO make static ?
+        @SuppressWarnings("ConstantConditions") String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
         String token = FirebaseInstanceId.getInstance().getToken();
         FirebaseDatabase.getInstance().getReference("Users").child(uid).child("deviceToken").setValue(token);
     }
 
-    public void removeToken(){
+    public void removeToken(){ //TODO make static ?
+        //noinspection ConstantConditions
         String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
         String token = FirebaseInstanceId.getInstance().getToken();
         if(token == null) return;
         // TODO add listeners
         FirebaseDatabase.getInstance().getReference("Users").child(uid).child("deviceToken").removeValue();
+    }
+
+    static public void uploadIdentityAnswer(Context context, boolean isApproved, String verificationUID){
+        //noinspection ConstantConditions
+        FirebaseDatabase.getInstance()
+                .getReference("IdentityVerifications")
+                .child(verificationUID)
+                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .setValue(isApproved);
+        Toast.makeText(context,
+                isApproved? "Identity approved successfully" : "Identity was not approved"
+                , Toast.LENGTH_SHORT).show();
     }
 }
