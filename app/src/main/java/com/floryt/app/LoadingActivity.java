ea2095024384d17c3d08b8ace7600e31f4a68462
Steven Dashevsky <stiven1999d@gmail.com>
2017-06-11 00:14:58 +0300

    Implemented new splash screen

--- app/src/main/java/com/floryt/app/LoadingActivity.java
+++ app/src/main/java/com/floryt/app/LoadingActivity.java
@@ -4,8 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.animation.Animation;
-import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -13,7 +14,7 @@ import com.google.firebase.auth.FirebaseAuth;
 public class LoadingActivity extends AppCompatActivity {
 
     private static final String TAG = "LoginActivity";
-    private static final long SPLASH_DISPLAY_LENGTH = 500;
+    private static final long SPLASH_DISPLAY_LENGTH = 750;
     ImageView imageView;
 
     @Override
@@ -21,13 +22,12 @@ public class LoadingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
 
-        imageView = (ImageView)findViewById(R.id.logo);
-        RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-        rotate.setDuration(SPLASH_DISPLAY_LENGTH);
-        imageView.setAnimation(rotate);
+        View v = findViewById(R.id.purple_circle);
+        ScaleAnimation fade_in =  new ScaleAnimation(0f, 100f, 0f, 100f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        fade_in.setDuration(700);     // animation duration in milliseconds
+        fade_in.setFillAfter(true);    // If fillAfter is true, the transformation that this animation performed will persist when it is finished.
+        v.startAnimation(fade_in);
 
-        /* New Handler to start the Menu-Activity
-         * and close this Splash-Screen after some seconds.*/
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
